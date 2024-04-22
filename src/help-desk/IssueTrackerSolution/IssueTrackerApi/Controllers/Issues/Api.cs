22fe37d1b4077093926584d3017b31e82b9b4223
Eric Chen <eric.chen5@yahoo.com>
2024-04-22 15:19:28 -0400

    Before break synced with jeff

---
+++ src/help-desk/IssueTrackerSolution/IssueTrackerApi/Controllers/Issues/Api.cs
@@ -0,0 +1,88 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IssueTrackerApi.Controllers.Issues;
+
+public class Api : ControllerBase
+{
+
+    // GET /issues
+    [HttpGet("/issues")]
+    public async Task<ActionResult> GetTheIssuesAsync()
+    {
+        var issues = new List<object>();
+        return Ok(issues);
+    }
+
+    [HttpPost("/issues")]
+    public async Task<ActionResult> AddIssueAsync(
+       [FromBody] CreateIssueRequestModel request,
+       [FromServices] IValidator<CreateIssueRequestModel> validator)
+    {
+        var results = await validator.ValidateAsync(request);
+        if (results.IsValid)
+        {
+            var response = new Issue
+            {
+                CreatedAt = DateTimeOffset.UtcNow,
+                Description = request.Description,
+                Software = request.Software,
+                Id = Guid.NewGuid(),
+                Status = IssueStatus.Created
+            };
+            // do our thing.
+            return Ok(response);
+        }
+        else
+        {
+            return BadRequest(results.ToDictionary()); // 400
+        }
+
+    }
+}
+
+/*
+ * {
+  "software": "Excel",
+  "description": "I want clippy back"
+} */
+
+public record CreateIssueRequestModel
+{
+
+    public string Software { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
+
+
+public record Issue
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Software { get; set; } = string.Empty;
+    public DateTimeOffset CreatedAt { get; set; }
+    public IssueStatus Status { get; set; }
+
+}
+public enum IssueStatus { Created }
+
+public class CreateIssueRequestModelValidator : AbstractValidator<CreateIssueRequestModel>
+{
+    private readonly IReadOnlyList<string> _supportedSoftware = ["excel", "powerpoint", "word"];
+    public CreateIssueRequestModelValidator()
+    {
+        RuleFor(i => i.Description)
+            .NotEmpty()
+            .MaximumLength(1024);
+
+        RuleFor(i => i.Software)
+            .NotEmpty()
+            .Must(i =>
+            {
+                var sw = i.ToLowerInvariant().Trim();
+                return _supportedSoftware.Any(s => s == sw);
+
+            }).WithMessage("Unsupported Software. Good Luck");
+
+    }
+}
---
+++ src/help-desk/IssueTrackerSolution/IssueTrackerApi/Program.cs
@@ -0,0 +1,32 @@
+using FluentValidation;
+using IssueTrackerApi.Controllers.Issues;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddValidatorsFromAssemblyContaining<CreateIssueRequestModelValidator>();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// the stuff above this line - configuration of the api and all the stuff inside of it.
+var app = builder.Build();
+// the stuff after this line. - it is how it takes requests and makes them into responses.
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers(); // Use .NET Reflection to go find all the routes to create the route table.
+
+app.Run(); // This is a "blocking" message pump.
